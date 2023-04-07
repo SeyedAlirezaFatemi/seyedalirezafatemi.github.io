aa3ae88b3621afdc82c29960c214ab171fe2b24a
Alireza Fatemi <alirezafa@rocketmail.com>
2023-04-07 16:19:08 +0300

    feat: add custom header for sections

---
+++ src/components/Typography/Headers.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export function SectionHeader({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1 className={twMerge('text-2xl font-semibold uppercase', className)}>
+      {children}
+    </h1>
+  );
+}
