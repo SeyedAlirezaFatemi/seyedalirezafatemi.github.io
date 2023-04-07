9ebfb78b251e8e280e87aaf6d1f1441ac21ad1c6
Alireza Fatemi <alirezafa@rocketmail.com>
2023-04-07 16:20:04 +0300

    style: change background color for light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms")({
-      strategy: "class",
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')({
+      strategy: 'class',
     }),
-    require("daisyui"),
-
+    require('daisyui'),
   ],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'],
+          'base-100': 'rgb(248 250 252)',
+        },
+      },
+      'dark',
+    ],
   },
 };
