04af1adc3b52155cc58450894baa3c68e66f5167
Alireza Fatemi <alirezafa@rocketmail.com>
2023-04-07 16:29:39 +0300

    feat: add WorkItem component

--- src/components/Sections/WorkSection.tsx
+++ src/components/Sections/WorkSection.tsx
@@ -1,43 +1,65 @@
+import React from 'react';
 import { twMerge } from 'tailwind-merge';
 import styles from './work.module.css';
+import Nokia from '@/assets/Nokia.svg';
+import { SectionHeader } from '@/components/Typography/Headers';
+import Image from 'next/image';
 
 export default function WorkSection() {
   return (
     <div className="text-center">
-      <h1>Work & Research Experiences</h1>
-      <span
-        className={twMerge(
-          'block w-fit font-semibold uppercase tracking-wide text-inherit no-underline',
-          'bg-gradient-to-r from-sky-500 to-indigo-500 bg-no-repeat',
-          styles.link
-        )}
+      <SectionHeader className="mb-8 text-center">
+        Work & Research Experiences
+      </SectionHeader>
+      <WorkItem
+        imageSrc={Nokia}
+        company="Nokia, Advanced Technology Group"
+        date="May 2022 - November 2022"
+        title="Software Developer Trainee"
       >
-        Nokia, Advanced Technology Group
-      </span>
+        I worked as a trainee in the Advanced Technology Group. My focus was on
+        Augmented Reality and different frameworks used for AR development. I
+        also did research on visual localization.
+      </WorkItem>
     </div>
   );
 }
 
-/**
- * link: {
-    textDecoration: "none",
-    color: "inherit",
-    display: "block",
-    textTransform: "uppercase",
-    fontWeight: 600,
-    letterSpacing: 1.2,
-    paddingTop: "2rem",
-    width: "fit-content",
-    "&:link": {
-      color: "inherit",
-    },
-    backgroundImage: "linear-gradient(to right, #00c6ff, #0072ff)",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "100% 0.2em",
-    backgroundPosition: "0 100%",
-    transition: "background-size 0.25s ease-in",
-    "&:hover": {
-      backgroundSize: "100% 50%",
-    },
-  },
- */
+function WorkItem({
+  imageSrc,
+  title,
+  company,
+  date,
+  children,
+}: {
+  children: React.ReactNode;
+  company: string;
+  date: string;
+  imageSrc: string;
+  title: string;
+}) {
+  return (
+    <div className="card w-full flex-wrap bg-base-100 text-start shadow-xl">
+      <figure className="h-32 w-full">
+        <Image src={imageSrc} alt="Nokia" className="w-6/12" />
+      </figure>
+      <div className="card-body p-4">
+        <h3
+          className={twMerge(
+            'block w-fit text-center font-semibold uppercase tracking-wide no-underline',
+            'bg-gradient-to-r from-sky-500 to-indigo-500 bg-no-repeat',
+            styles.link
+          )}
+        >
+          {company}
+        </h3>
+        <div className="flex">
+          <h4 className="text-sm font-medium">{title}</h4>
+          <div className="divider divider-horizontal" />
+          <span className="text-thin text-sm">{date}</span>
+        </div>
+        <p className="text-justify text-sm font-normal">{children}</p>
+      </div>
+    </div>
+  );
+}
