a4c8040b9fd7dbeb126e669a2d64a35a76cce7ea
Ali Heydari <ali4heydari@gmail.com>
2023-06-04 20:43:44 +0430

    feat: blog feature first draft

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import type { GetStaticPaths, GetStaticProps } from 'next';
+import type { BlogPost } from '@/features/blog/@types';
+import { getBlogPost, getBlogPosts } from '@/utils/notion';
+
+interface Props {
+  blogPost: BlogPost;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const blogPosts = await getBlogPosts();
+  const paths = blogPosts.map((post: any) => ({ params: { slug: post.slug } }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const blogPost = await getBlogPost(params?.slug as string);
+
+  return { props: { blogPost } };
+};
+
+export default function BlogPostPage({ blogPost }: Props) {
+  return (
+    <article className="container mx-auto">
+      <div className="flex">
+        <h2 className="mb-4 text-4xl font-bold">{blogPost.title}</h2>
+        <div className="ml-4 flex items-center">
+          <div className="mx-4 text-gray-500">|</div>
+          <p className="text-gray-500">
+            {new Date(blogPost.created).toLocaleDateString('en-GB')}
+          </p>
+          <div className="mx-4 text-gray-500">|</div>
+          {blogPost.authors.map((author) => (
+            <p key={author} className="text-gray-500">
+              {author}
+            </p>
+          ))}
+        </div>
+      </div>
+      <ReactMarkdown
+        linkTarget="_blank"
+        className="prose max-w-none text-justify leading-tight xl:prose-lg 2xl:prose-xl"
+      >
+        {blogPost.content as string}
+      </ReactMarkdown>
+      <div>
+        <div>
+          <h3 className="mb-4 mt-8 text-2xl font-bold">Tags</h3>
+        </div>
+        <div className="mt-2 flex items-center">
+          {blogPost.tags.map((tag) => (
+            <span
+              key={tag}
+              className="mr-2 rounded-full bg-gray-200 px-2 py-1 text-sm text-gray-700"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </article>
+  );
+}
---
+++ src/utils/notion.ts
@@ -0,0 +1,59 @@
+import { n2m, notionClient } from '@/components/Notion/client';
+import type { BlogPost, NotionBlogDto } from '@/features/blog/@types';
+
+export async function getBlogPosts(): Promise<BlogPost[]> {
+  const databaseId = process.env.BLOG_DATABASE_ID as string;
+  const response = await notionClient.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: 'published',
+      checkbox: {
+        equals: true,
+      },
+    },
+  });
+
+  return (response.results as NotionBlogDto[]).map((page) => ({
+    title: page.properties.title.title[0].text.content,
+    created: page.properties.created.created_time,
+    updated: page.properties.updated.last_edited_time,
+    authors: page.properties.author.people.map((it) => it.object),
+    description: page.properties.description.rich_text[0].text.content,
+    slug: page.properties.slug.formula.string,
+    published: page.properties.published.checkbox.valueOf(),
+    tags: page.properties.tags.multi_select.map((tag) => tag.name),
+  }));
+}
+
+export async function getBlogPost(slug: string): Promise<BlogPost> {
+  const databaseId = process.env.BLOG_DATABASE_ID as string;
+  const response = await notionClient.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: 'slug',
+      formula: {
+        string: {
+          equals: slug,
+        },
+      },
+    },
+  });
+
+  const page = response.results[0] as NotionBlogDto;
+
+  const pageMarkdown = await n2m.pageToMarkdown(page.id);
+
+  const content = n2m.toMarkdownString(pageMarkdown);
+
+  return {
+    title: page.properties.title.title[0].text.content,
+    created: page.properties.created.created_time,
+    updated: page.properties.updated.last_edited_time,
+    authors: page.properties.author.people.map((it) => it.object),
+    description: page.properties.description.rich_text[0].text.content,
+    slug: page.properties.slug.formula.string,
+    published: page.properties.published.checkbox.valueOf(),
+    content,
+    tags: page.properties.tags.multi_select.map((tag) => tag.name),
+  };
+}
